e2e2f639f82a5ecd558c6d8788ade46f15c7acd6
Rakhshan-mab-labs <rakhshan@mab-labs.com>
2025-07-24 11:37:48 -0400

    Assert Failure from @ WEST_TOPDIR/modules/lib/gui/lvgl/src/misc/cache/lv_cache_lre_rb.c:190

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/display.h>
+#include <lvgl.h>
+#include "examples/lv_example_get_started.h"
 
-int main(void)
+void main(void)
 {
-        return 0;
+    const struct device *display = DEVICE_DT_GET_ANY(ssd1680);
+    if (!device_is_ready(display)) {
+        printk("Display not ready\n");
+        return;
+    }
+
+    display_blanking_off(display);
+
+    /* Run LVGL example */
+    lv_example_get_started_1();
+
+    k_msleep(2000);
+
+    while (1) {
+        lv_task_handler();
+        k_msleep(100);
+    }
 }
